f11669f1d136a7c490cb0ddfea5d1f493b9222bb
otsoweckstrom <97otso@gmail.com>
2025-04-24 17:43:03 +0300

    rm unnecessary library

--- ML_model_testing/ML_model_final_crossvalidation.py
+++ ML_model_testing/ML_model_final_crossvalidation.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix # Added confusion matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix 
 import matplotlib.pyplot as plt
 import seaborn as sns 
 from imblearn.over_sampling import SMOTE
